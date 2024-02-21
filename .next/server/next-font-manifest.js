cbd348fc6ce9eea452cacbf848fc64d208a474ca
Mohsin853 <61412070+Mohsin853@users.noreply.github.com>
2024-02-21 22:18:56 +0530

    Delete .next directory

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/user/Videos/btst-blog-app-submission/app/layout\":[\"static/media/c22ccc5eb58b83e1-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
