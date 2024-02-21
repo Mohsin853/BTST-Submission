e979f3a2b08f1926a50b45c451b46b9f0164c4ac
Mohsin853 <61412070+Mohsin853@users.noreply.github.com>
2024-02-21 22:27:37 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
     <main>
       <h1>Dashboard</h1>
       <p>Hi!</p>
-      <p>I am Mohsin Shanavas. Welcome to my blog app submission for BTST. I have coded this using Next.Js. Dummy data for blog posts have been added to db.json in the _data directory. You can also create your own blog posts by moving to 'Create Blog' page. Feel free to surf through the page.</p>
+      <p>I am Mohsin Shanavas. Welcome to my blog app submission for BTST. I have coded this using Next.Js. Dummy data for blog posts have been added to db.json in the _data directory. You can also create your own blog posts by moving to Create Blog page. Feel free to surf through the page.</p>
       <p>Hoping to hear from you soon :)</p>
 
       <div className="flex justify-center my-8">
